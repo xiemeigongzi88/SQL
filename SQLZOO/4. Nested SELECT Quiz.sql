6ea2e8c4eab227f6e83ef5a1a1faf42d047324bf
xiemeigongzi88 <43561060+xiemeigongzi88@users.noreply.github.com>
2019-08-23 20:56:39 -0500

    Add files via upload

---
+++ SQLZOO/4. Nested SELECT Quiz.sql
@@ -0,0 +1,16 @@
+4. Nested SELECT Quiz
+
+bbc
+name	region	area	population	gdp
+Afghanistan	South Asia	652225	26000000	
+Albania	Europe	28728	3200000	6656000000
+Algeria	Middle East	2400000	32900000	75012000000
+Andorra	Europe	468	64000	
+Bangladesh	South Asia	143998	152600000	67144000000
+United Kingdom	Europe	242514	59600000	2022824000000
+...
+
+/*
+1.  Select the code that shows the name, region and population of the smallest country in each region
+*/
+
